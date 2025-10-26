88788607298bdf0ff7b2167e8ca677408bf08e8c
paxerr <quyetnguyenst2005@gmail.com>
2025-10-26 20:56:25 +0700

    add layout chung

--- GG_Shop/App_Start/BundleConfig.cs
+++ GG_Shop/App_Start/BundleConfig.cs
@@ -1,30 +1,24 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
-namespace GG_Shop
+public class BundleConfig
 {
-    public class BundleConfig
+    public static void RegisterBundles(BundleCollection bundles)
     {
-        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
-        public static void RegisterBundles(BundleCollection bundles)
-        {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+        // CSS Bundle
+        bundles.Add(new StyleBundle("~/Content/css").Include(
+            "~/Content/css/bootstrap.min.css",
+            "~/Content/css/font-awesome.min.css",
+            "~/Content/css/elegant-icons.css",
+            "~/Content/css/nice-select.css",
+            "~/Content/css/owl.carousel.min.css",
+            "~/Content/css/style.css"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new Bundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
-        }
+        // JS Bundle
+        bundles.Add(new ScriptBundle("~/bundles/malefashion").Include(
+            "~/Scripts/jquery-3.3.1.min.js",
+            "~/Scripts/bootstrap.min.js",
+            "~/Scripts/jquery.nice-select.min.js",
+            "~/Scripts/owl.carousel.min.js",
+            "~/Scripts/main.js"));
     }
 }
